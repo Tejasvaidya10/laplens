5524238b660dd8567954f0b5af2b27f1d06e758e
Tejas Vaidya <vaidyatejas02@gmail.com>
2025-12-21 20:21:31 -0500

    Fix SavedAnalyses export

--- apps/web/src/pages/index.ts
+++ apps/web/src/pages/index.ts
@@ -1,3 +1,4 @@
 export { Dashboard } from './Dashboard';
 export { SavedAnalyses } from './SavedAnalyses';
 export { AuthCallback } from './AuthCallback';
+export { default as SavedAnalyses } from './SavedAnalyses';
